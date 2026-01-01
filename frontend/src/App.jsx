a9152ce62b3437565c4bfea137ac6f657ca2d8a0
Hemil Patel <hemil932hemil@gmail.com>
2026-01-01 16:52:30 +0530

    Add initial dashboard UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
+import Dashboard from "./pages/Dashboard/Dashboard";
 export default function App() {
-  return (
-    <div className="min-h-screen bg-green-500 flex items-center justify-center">
-      <h1 className="text-blue-600 text-5xl font-extrabold">
-        TAILWIND TEST
-      </h1>
-    </div>
-  );
+  return <Dashboard />;
 }

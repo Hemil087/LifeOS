9b8ead5829ae16a757bfffa8ff6ca01b1580bdbd
Hemil Patel <hemil932hemil@gmail.com>
2026-01-08 23:03:00 +0530

    Altered icon file

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.gjkf6m8egjo"
+    "revision": "0.of102228iho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
